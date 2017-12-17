a877894b22202c01a4e71f88b1bdeda1c7d5986a
Ton <conaron@gmail.com>
2017-12-17 04:04:18 -0200

    xxx

--- src/main/java/controller/Login.java
+++ src/main/java/controller/Login.java
@@ -6,9 +6,9 @@ package controller;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
-import javafx.event.ActionEvent;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 
 /**
  *
