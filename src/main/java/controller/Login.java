28912fb75f9bea79383a730d4f62a509cf76dc43
Airton <conaron@gmail.com>
2017-12-18 17:32:28 -0200

    XXX

--- src/main/java/controller/Login.java
+++ src/main/java/controller/Login.java
@@ -3,50 +3,56 @@
  */
 package controller;
 
+import java.util.Map;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
- 
+
 import org.primefaces.context.RequestContext;
- 
+
 @ManagedBean
 public class Login {
-     
+
     private String username;
-     
     private String password;
- 
+
     public String getUsername() {
         return username;
     }
- 
+
     public void setUsername(String username) {
         this.username = username;
     }
- 
+
     public String getPassword() {
         return password;
     }
- 
+
     public void setPassword(String password) {
         this.password = password;
     }
-   
+
     public void login(ActionEvent event) {
-        RequestContext context = RequestContext.getCurrentInstance();
+        FacesContext context = FacesContext.getCurrentInstance();
+        RequestContext request = RequestContext.getCurrentInstance();
+
+        Map<String, String> requestParamMap = context.getExternalContext().getRequestParameterMap();
+        String hash = requestParamMap.get("hash");
+
         FacesMessage message = null;
         boolean loggedIn = false;
-         
-        if(username != null && username.equals("admin") && password != null && password.equals("admin")) {
-            loggedIn = true;
-            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
-        } else {
-            loggedIn = false;
-            message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Loggin Error", "Invalid credentials");
-        }
-         
+
+        message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Teste de Hash", "Hash: " + hash);
+//        if (username != null && username.equals("admin") && password != null && password.equals("admin")) {
+//            loggedIn = true;
+//            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
+//        } else {
+//            loggedIn = false;
+//            message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Loggin Error", "Invalid credentials");
+//        }
+
         FacesContext.getCurrentInstance().addMessage(null, message);
-        context.addCallbackParam("loggedIn", loggedIn);
-    }   
+        request.addCallbackParam("loggedIn", loggedIn);
+    }
 }
